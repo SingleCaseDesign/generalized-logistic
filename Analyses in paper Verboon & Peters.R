8fd4ceb9d72dbf2e5758e114089e40e54d4929b7
PeterVerboon <Peter.Verboon@ou.nl>
2017-10-13 16:36:09 +0200

    cleaning script

--- Analyses in paper Verboon & Peters.R
+++ Analyses in paper Verboon & Peters.R
@@ -15,6 +15,7 @@ options(digits=4);
 # Ab = asymptotic base of the function
 # At = asymptotic top of the function
 # B  = scaling factor, controls steepness of curve (growth rate)
+# v  = not used in this study (fixed at v = 1)
 
 # Define formula for S-curve: generalized logistic 
 
@@ -23,7 +24,7 @@ GLF <- "y ~ Ab + (At - Ab)/ (1 + exp(-B*(x-x0)))**(1/v)"    # formula S-curve wi
 
 
 
-####  Definition Generalized Logistic function (NB "B" is in exp()) with scaling factor
+####  Definition Generalized Logistic function with scaling factor
 
 genlogf <- function(x, x0 = 10, Ab = 1, At=7, B = 0.5, v = 1) {
   
@@ -43,19 +44,33 @@ nB <- 30;         # nB : number of measurement in phase B
 x <- c(1:nB)
 y <- genlogf(x, x0=10, Ab=1.5, At=6.5, B = 0.3, v = 1) 
 
-yA <- rnorm(nA, 1.5, 0.5)                                   # adjust first observations in A phase to be without effect
+yA <- rnorm(nA, 1.5, 0.5)                                   # the first observations in A phase are without effect
 
 y <- c(yA,y)
 sdy <- sd(y)
 
 x <- c(1:(nA+nB))
-y <- y + rnorm(length(x),0, 0.5*sdy)                        # construct data with random error: dependent varable
-y[y < 1] <- 1;  y[y > 7] <- 7                               # floor and ceiling effects
+y <- y + rnorm(length(x),0, 0.5*sdy)                        # add random error to curve: dependent varable
+y[y < 1] <- 1;  y[y > 7] <- 7                               # add hard floor and ceiling effects
 
 
 dat <- data.frame(x=x,y=y)
-a <- data.frame(x = (31:35),y = rnorm(5, 6.5, 0.2) )        # add aditional error in last five points
+a <- data.frame(x = (31:35),y = rnorm(5, 6.5, 0.2) )        # add aditional random error in last five points
 dat1 <- rbind(dat,a)
+
+dat1$Phase <- c(rep(1,5),rep(2,30))
+
+meanDiff(x=dat1$Phase, y= dat1$y)                  ## simple test of phase difference
+
+write.table(dat1, "datExample1.txt", sep="\t")     ## save example outside R
+
+
+######## End data construction for example
+
+
+
+dat1 <- read.table("datExampl1.txt",sep="\t")   
+dat1$Phase <- dat1$Phase - 1 
 
 
 # Figure 1 from paper 
@@ -63,24 +78,7 @@ dat1 <- rbind(dat,a)
 g <- ggplot(dat1) + geom_point(aes(x=x,y=y))                
 g  
 
-dat1$Phase <- c(rep(1,5),rep(2,30))
-meanDiff(x=dat1$Phase, y= dat1$y)                  ## simple test of phase difference
-
-#  datExample1 <- dat1                             ## example data in paper Verboon & Peters
- 
-write.table(dat1, "datExample1.txt", sep="\t")       ## save example outside R
-
-
-dat1 <- datExample1                                      
-
-##  vbdat <- read.table("datExampl1.txt",sep="\t")
-##  vbdat$Phase <- vbdat$Phase -1 
-
-
-######## End data construction for example
-
-
-######## example: linear fit 
+### Example: linear fit 
 
 lmout <- lm(dat1$y ~ dat1$x)
 coef(lmout)
@@ -100,30 +98,26 @@ g
 
 # Figure 3 from paper
 
-p <- qplot(data = dat1, x, res, xlab = "measurements points", ylab = "residuals")
-g <- p + geom_smooth(method = "loess", size = 1.0)
+g <- qplot(data = dat1, x, res, xlab = "measurements points", ylab = "residuals")
+g <- g + geom_smooth(method = "loess", size = 1.0)
 g
 
 
-
-### example: end linear fit
-
+### End example: linear fit
 
 
-########### ES PWREG
 
-library(lsr)     # tools for students
+### Example Piecewise Regression
 
 # dat1$Phase <- c(rep(1,5),rep(2,30))
 # ypre <- dat1[c(1:5),"y"]
 # ypost <- dat1[c(6:35),"y"]
 
-meanDiff(x=dat1$Phase, y= dat1$y)
-cohensD(y~Phase, data=dat1)
+meanDiff(x=dat1$Phase, y= dat1$y)                 ## simple test of phase difference
 
-dat1$x0 <- dat1$x - 1
+dat1$x0 <- dat1$x - 1                             ## first observation index is set to zero 
 nA <- 5
-out <- PWreg(dat1[,c("x0","y")],nA = 5, robust=FALSE)        ## piecewise regression
+out <- PWreg(dat1[,c("x0","y")],nA = 5, robust=FALSE)        ## piecewise regression, uses function PWreg
 yfit <- out[[2]]$fitted.values
 ypre <- out[[2]]$coefficients[1] + out[[2]]$coefficients[3]*dat1[c(1:6),"x0"]
 
@@ -133,16 +127,15 @@ confint(out[[2]], c("D","Tc","X3"), level = 0.95)
 
 # Figure 4 from paper
 
-p <- qplot(data = dat1, x0, y, xlab = "measurements points", ylab = "scores")
-g <- p + geom_line(aes(x0, y=yfit, group=Phase))
+g <- qplot(data = dat1, x0, y, xlab = "measurements points", ylab = "scores")
+g <- g + geom_line(aes(x0, y=yfit, group=Phase))
 g <- g + geom_vline(xintercept= (nA-.5), colour="red", size = 1.5) 
 g <- g + geom_segment(aes(x=nA, xend=nA, y=yfit[(nA+1)], yend=ypre[(nA+1)]),linetype=1, colour = "green", size=1)
 g <- g + geom_segment(aes(x=(nA-1), xend=nA, y=ypre[nA], yend=ypre[(nA+1)]),colour="blue", linetype = "dashed")
 g <- g + geom_smooth(method="lm", aes(group = Phase))
-# g <- g + annotate("text", x = 5.2, y = 1.3, label = "level effect", colour = "black", size=4)
 g
 
-
+### Linear regression per phase
 
 out0 <- lm(dat1[c(1:5),"y"] ~ dat1[c(1:5),"x"])
 coef(out0)
@@ -153,9 +146,7 @@ coef(out1)
 confint(out1, level = 0.95)
 
 
-### example to show phase effect
-
-# vbdat2 <- dat2
+### Example to show phase effect
 
 a <- rnorm(5, 1.5, 0.8)
 b <- rnorm(15, 3.0, 0.8)
@@ -169,7 +160,6 @@ ypost <- dat2[c(6:35),"y"]
 
 meanDiff(x=dat2$phase, y= dat2$y)
 
-
 out <- PWreg(dat2[,c("x0","y")],nA = 5, robust=FALSE)  ## piecewise regression
 yfit <- out[[2]]$fitted.values
 ypre <- out[[2]]$coefficients[1] + out[[2]]$coefficients[3]*dat2[c(1:6),"x0"]
@@ -178,25 +168,22 @@ confint(out[[2]], c("D","Tc","X3"), level = 0.95)
 
 # Figure 5 from paper 
 
-p <- qplot(data = dat2, x0, y, xlab = "measurements points", ylab = "scores" )
-g <- p + geom_line(aes(x0, y=yfit, group=phase))
+g <- qplot(data = dat2, x0, y, xlab = "measurements points", ylab = "scores" )
+g <- g + geom_line(aes(x0, y=yfit, group=phase))
 g <- g + geom_vline(xintercept= (nA-.5), colour="red", size = 1.5) 
 g <- g + geom_segment(aes(x=nA, xend=nA, y=yfit[(nA+1)], yend=ypre[(nA+1)]),linetype=1, colour = "green", size=1)
 g <- g + geom_segment(aes(x=(nA-1), xend=nA, y=ypre[nA], yend=ypre[(nA+1)]),colour="blue", linetype = "dashed")
 g <- g + geom_smooth(method="lm", aes(group = phase))
 g <- g + scale_y_continuous(limits = c(0,6))
- g <- g + annotate("text", x = 6.5, y = 2.6, label = "level effect", colour = "black", size=4)
+g <- g + annotate("text", x = 6.5, y = 2.6, label = "level effect", colour = "black", size=4)
 g
 
 
 
-###  example Genlog function
-
-dat1 <- vbdat
+###  Example genlog function
 
 
 # Figure 6 paper
-
 
 genlog(dat1, Xs=10, Bs=.5, ABs = 1, ATs = 7 , range=8,plot=TRUE)
 
